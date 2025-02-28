22bbe04dedb2dbd68a8bbb10edc0e3cb0a85804f
Silvio Calixto <112583090+SilvioCalixto@users.noreply.github.com>
2025-02-28 15:16:24 -0300

    atualizei o mobile

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-import 'animate.css';
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('menu');
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  })
+
+}
+
+  if (close) {
+    close.addEventListener('click', () => {
+      nav.classList.remove('active');
+    });
+}
